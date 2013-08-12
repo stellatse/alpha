50e843998386ae112da811fe6ecfc6f4134db80c
Stella X <stellatsecyin@gmail.com>
2013-08-12 15:33:58 +0800

    update models

--- tmp/account/models.py
+++ tmp/account/models.py
@@ -5,5 +5,30 @@ import os.path
 
 class CustomerAccount(models.Model):
 	"""Customer account info records"""
+	ACCOUNT_TYPE = (
+		('INC', 'Increase'),
+		('DEC', 'Decrease'),
+		('NUL', 'Nothing'),
+		)
 	customer = models.ForeignKey(Customer)
-	
+	a_type = models.CharField(max_length=3, choices=ACCOUNT_TYPE)
+	number = models.IntegerField()
+	result = models.IntegerField()
+	operator = models.ForeignKey(Admin)
+	udpate_time = models.DateTimeField(auto_now=True)
+
+
+class SupplierAccount(models.Model):
+	"""Supplier account info records"""
+	ACCOUNT_TYPE = (
+		('INC', 'Increase'),
+		('DEC', 'Decrease'),
+		('NUL', 'Nothing'),
+		)
+	supplier = models.ForeignKey(Supplier)
+	a_type = models.CharField(max_length=3, choices=ACCOUNT_TYPE)
+	number = models.IntegerField()
+	result = models.IntegerField()
+	operator = models.ForeignKey(Admin)
+	udpate_time = models.DateTimeField(auto_now=True)
+		
