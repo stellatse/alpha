067d43c48874dea5dc990be158427f10c25eedd7
Lu Jun <luj1985@gmail.com>
2013-08-13 23:53:52 +0800

    update home page, add login support

--- unk/urls.py
+++ unk/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from unk.register import views
+import unk.views
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    #url(r'^$', 'unk.views.home', name='home'),
-    url(r'^$', views.home, name='home'),
+    url(r'^$', unk.views.home, name='home'),
     url(r'^register/', include('unk.register.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
