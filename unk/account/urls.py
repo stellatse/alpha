4b3a1fd25ebeca1d664b8a9de37afc46ba21567e
Lu Jun <luj1985@gmail.com>
2013-08-14 00:27:01 +0800

    Add django auth support

--- unk/account/urls.py
+++ unk/account/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls import patterns, url
 from unk.account import views
 
 urlpatterns = patterns('',
-	url(r'^login/', views.login, name='login'),
-
+	url(r'^login/', 'django.contrib.auth.views.login', name='login'),
 	)
 
--- unk/account/views.py
+++ unk/account/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 
 def home(request):
-    return render(request, 'register/index.html')
+    return render(request, 'account/index.html')
 
 # json request ?
 def login(request):
+    username = 'admin'
+    password = 'admin'
+    user = authenticate(username, password)
+    if user is not None:
+        # the password verified for the user
+        if user.is_active:
+            print("User is valid, active and authenticated")
+        else:
+            print("The password is valid, but the account has been disabled!")
+    else:
+        # the authentication system was unable to verify the username and password
+        print("The username and password were incorrect.")
     context = {}
-    return render(request, 'register/login.html', context)
+    return render(request, 'index.html', context)
+
+def logout(request):
+    return render(request, '')
