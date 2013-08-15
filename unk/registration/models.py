81843e2b913a3c07597875140d0d4f89c4ecdeae
Stella X <stellatsecyin@gmail.com>
2013-08-15 14:08:34 +0800

    change model

--- unk/registration/models.py
+++ unk/registration/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.utils.translation import ugettext_lazy as _, pgettext_lazy
 class Country(models.Model):
     """
     International Organization for Standardization (ISO) 3166-1 Country list.
