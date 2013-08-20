f85a16b4901592efd07e3308775c430b146d274a
Stella X <stellatsecyin@gmail.com>
2013-08-20 17:43:48 +0800

    update reg

--- unk/views.py
+++ unk/views.py
@@ -1,7 +1,7 @@
 from unk.registration.models import Country
 from django.shortcuts import render
-
+import pytz
 def home(request):
     countries = Country.objects.all()
-    context = {'countries':countries}
+    context = {'countries':countries, 'timezones': pytz.common_timezones}
     return render(request, 'unk/index.html', context)
