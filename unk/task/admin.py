cc62539556c644f0a1cd3e82c8a8012318848af1
Stella X <stellatsecyin@gmail.com>
2013-08-21 19:31:43 +0800

    update task model admin

--- unk/task/admin.py
+++ unk/task/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from unk.account.models import CustomerAccount, SupplierAccount
 from unk.registration.models import Customer, Supplier
-from unk.task.models import Service, Currency
+from unk.task.models import Service, Currency, ServiceCategory
 
-admin.site.register(Currency)
+class CurrencyAdmin(admin.ModelAdmin):
+	fields = ['name', 'code', 'symbol']
+
+
+admin.site.register(Currency, CurrencyAdmin)
 admin.site.register(Service)
-
+admin.site.register(ServiceCategory)
 		
