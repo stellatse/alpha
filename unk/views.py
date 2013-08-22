1d75e018921d5db164d857a179cc16144452ef30
Stella X <stellatsecyin@gmail.com>
2013-08-22 13:52:58 +0800

    update login info and logout view

--- unk/views.py
+++ unk/views.py
@@ -1,6 +1,6 @@
 from unk.registration.models import Country
 from unk.registration.forms import RegistrationForm
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 #import pytz
 
@@ -16,9 +16,4 @@ def home(request):
 
 def logout_view(request):
     logout(request)
-    countries = Country.objects.all()
-    form = RegistrationForm({'timezone': 'UTC'})
-    return render(request, 'unk/index.html', {
-        'countries':countries, 
-        'form': form
-    })
+    return redirect('home')
