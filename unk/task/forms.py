96003ad50102c6cf2bb9383cc741d933a62a9259
Lu, Jun <luj1985@gmail.com>
2013-08-24 22:53:19 +0800

    Add order service page

---
+++ unk/task/forms.py
@@ -0,0 +1,6 @@
+from django.forms import ModelForm
+from unk.task.models import Task
+
+class TaskForm(ModelForm):
+    class Meta:
+        model = Task
--- unk/task/views.py
+++ unk/task/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import Http404
 from django.contrib.auth.decorators import login_required
 from unk.task.models import Service
+from unk.task.forms import TaskForm
 
 def service_list(request):
 	services = Service.objects.all()
@@ -14,10 +15,11 @@ def service_list(request):
 def order_service(request, service_id):
 	try:
 		service = Service.objects.get(pk=service_id)
+		form = TaskForm()
 	except Service.DoesNotExist:
 		raise Http404
-	context = {'service': service}
-	if request.method == 'POST':
-		print "Submit order form handle"
+	context = {'service': service, 'form': form}
+#	if request.method == 'POST':
+#		print "Submit order form handle"
 	return render(request, 'unk/services/service_order.html', context)
 
